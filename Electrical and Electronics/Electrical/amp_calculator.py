f00410d96282d7ce8bd9ac99e3ea7678154b07cf
Windows X-Lite <abdulzoha786@gmail.com>
2026-08-24 11:13:49 +0300

    some root files updated

--- Electrical and Electronics/Electrical/amp_calculator.py
+++ Electrical and Electronics/Electrical/amp_calculator.py
@@ -1,26 +1,51 @@
 """
-Power = Watt
+Power = Watt (W)
 Current = Ampare (I)
-R = Resistence (ohm)
+Power fector = PF (0.8)
 W = Watt
 I = Current (Ampare)
-pf = Power fector
-1.732 = root(3)
+R = Resistence (ohm)
+V = Volt
+pf = 0.8
+root(3) = 1.732
+
 
 DC current
 ----------
+W = I*V
+V = 12V
+I = 200A
+2400W = 12*200
+
 I = W/V
+W = 2400W
+V = 12V
+200V = 2400/12
+
 
 AC current (Single phase)
 -------------------------
-I = W/(V*pf)
+V = 220V
+pf = 0.8
+P = 1000W
+I = P/(V*pf)
+
+5.69 = 1000W/(220V*0.8)
+
 
 AC current (Three phase)
 ------------------------
-I = W/(root(3)*V*pf)
+V = 415V
+pf = 0.08
+P = 1000W
+I = P/(root(3)*V*pf)
+
+1.74 = 1000/(1.732*415*0.8)
+
+
 
 R = V / I
-W = V * I
+V = R * I
 
 
 
